ea311e4b156c4d3d27f2de95fdafb002561271df
NOT-EVIL-Jester <Jesteremailreal@proton.me>
2026-08-31 05:43:01 -0400

    menu stuff

--- Scene/Play.gd
+++ Scene/Play.gd
@@ -1,15 +1,13 @@
 extends Sprite2D
 
 @export var amplitude_degrees: float = 15.0  # max swing angle
-@export var speed: float = 2.0               # oscillations speed
+@export var speed: float = 1              # oscillations speed
 
 var time: float = 0.0
-
-func _ready() -> void:
-	pass
 
 func _process(delta: float) -> void:
 	time += delta
 	rotation = lerp(rotation, deg_to_rad(amplitude_degrees) * sin(time * speed), 0.5)
-	scale.y = 0.5 - abs(0 - rotation)
-	scale.x = 0.4 + abs(0 - rotation)
+	scale.y = 1 - abs(0 - rotation)
+	scale.x = 1 + abs(0 - rotation)
+	rotation = 0
